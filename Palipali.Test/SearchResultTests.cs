d7c8b4792fb273da7c172feb545c5302abca3d9f
Andrew Chaa <andrew.chaa@huddle.com>
2013-03-01 09:58:20 +0000

    Restructured the application and test project

---
+++ Palipali.Test/SearchResultTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+
+namespace Palipali.Test
+{
+    public class SearchResultTests
+    {
+        private static MenuRepository _menuRepository;
+        private static IEnumerable<SearchResult> _searchResults;
+
+        private Establish context = () => _menuRepository = new MenuRepository();
+
+        private Because of = () => _searchResults = _menuRepository.List(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu));
+
+        private It should_have_the_list_of_menu_programs = () => _searchResults.ShouldNotBeEmpty();
+    }
+}
